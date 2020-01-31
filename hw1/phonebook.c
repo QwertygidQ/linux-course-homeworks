e2e32d19a4903e4bfdfbbb5f6c08e07e32732649
QwertygidQ <Qwertygid@gmail.com>
2020-01-31 21:05:31 +0300

    Alphabetically sorted the imports

--- hw1/phonebook.c
+++ hw1/phonebook.c
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
-#include <linux/init.h>
-#include <linux/module.h>
 #include <linux/device.h>
-#include <linux/kernel.h>
 #include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
 
