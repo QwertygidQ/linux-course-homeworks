5ca35b5b5541ad143f190690a8b4da1f038977f4
QwertygidQ <Qwertygid@gmail.com>
2020-01-31 19:56:00 +0300

    Argument parsing

--- hw1/phonebook.c
+++ hw1/phonebook.c
@@ -25,6 +25,7 @@ size_t      users_count = 0;
 static int           major_number;
 static char          user_buffer[BUFFER_SIZE] = {0}; // messages from the user
 static int           user_msg_size;
+static int           user_buffer_needs_parsing = 0;
 static char          device_buffer[BUFFER_SIZE] = {0}; // messages to the user
 static int           device_msg_size;
 static int           device_opened_count = 0;
@@ -47,6 +48,8 @@ static struct User new_user(const char *data);
 static ssize_t     find_user(const char *surname);
 static int         add_user(const struct User user);
 static int         remove_user(size_t index);
+
+static int parse_user_buffer(void);
 
 static int __init phonebook_init(void) {
     printk(KERN_INFO "Phonebook: initializing the module\n");
@@ -107,6 +110,23 @@ static int dev_open(struct inode *inode, struct file *file) {
 }
 
 static int dev_release(struct inode *inode, struct file *file) {
+    if (user_buffer_needs_parsing) {
+        if (parse_user_buffer()) { // parse_user_buffer returns 1 on error
+            device_buffer[0] = 0;
+            device_msg_size = 0;
+            printk(KERN_INFO "Phonebook: cleared the device buffer\n");
+        }
+        user_buffer_needs_parsing = 0;
+    } else {
+        device_buffer[0] = 0;
+        device_msg_size = 0;
+        printk(KERN_INFO "Phonebook: cleared the device buffer\n");
+    }
+
+    user_buffer[0] = 0;
+    user_msg_size = 0;
+    printk(KERN_INFO "Phonebook: cleared the user buffer\n");
+
     device_opened_count--;
     module_put(THIS_MODULE);
 
@@ -116,9 +136,9 @@ static int dev_release(struct inode *inode, struct file *file) {
 
 // Data path: device -> user
 static ssize_t dev_read(struct file *file, char __user *buffer, size_t len, loff_t *offset) {
-    int error_count;
+    int error_count, copy_len;
 
-    int copy_len = min(device_msg_size - *offset, len);
+    copy_len = min(device_msg_size - *offset, len);
     if (copy_len <= 0)
         return 0;
 
@@ -135,15 +155,18 @@ static ssize_t dev_read(struct file *file, char __user *buffer, size_t len, loff
 
 // Data path: user -> device
 static ssize_t dev_write(struct file *file, const char __user *buffer, size_t len, loff_t *offset) {
-    int error_count;
+    int error_count, copy_len;
 
-    int copy_len = min(BUFFER_SIZE - *offset, len);
+    user_buffer_needs_parsing = 0;
+
+    copy_len = min(BUFFER_SIZE - *offset, len);
     if (copy_len <= 0) {
         if (BUFFER_SIZE == *offset)
             printk(KERN_INFO "Phonebook: no more space left in the buffer, ignoring\n");
         else
             printk(KERN_INFO "Phonebook: nothing more to copy from user space to device\n");
 
+        user_buffer_needs_parsing = 1;
         return len;
     }
 
@@ -154,8 +177,10 @@ static ssize_t dev_write(struct file *file, const char __user *buffer, size_t le
     }
 
     *offset += copy_len;
-    user_msg_size = *offset;
-    user_buffer[BUFFER_SIZE - 1] = '\0';
+    user_msg_size = max(*offset - 1, 0);
+    user_buffer[user_msg_size] = 0;
+    user_buffer[BUFFER_SIZE - 1] = 0; // guarantees that the user buffer is zero-terminated
+    user_buffer_needs_parsing = 1;
     printk(KERN_INFO "Phonebook: received %zu characters from the user, new message size: %d\n", len, user_msg_size);
     return copy_len;
 }
@@ -224,7 +249,7 @@ static struct User new_user(const char *data) {
 static ssize_t find_user(const char *surname) {
     size_t i;
     for (i = 0; i < users_count; i++) {
-        if (strcmp(users[i].surname, surname) == 0)
+        if (strcmp(surname, users[i].surname) == 0)
             return i;
     }
 
@@ -255,6 +280,93 @@ static int remove_user(size_t index) {
         users[i] = users[i + 1];
 
     users_count--;
+    return 0;
+}
+
+/*
+* Format:
+* f surname -- get all user data by surname (finds the first user with this surname)
+* a name surname phone email age -- add a user
+* d surname -- remove a user by surname (finds the first user with this surname)
+*/
+static int parse_user_buffer(void) {
+    ssize_t index;
+    struct User user;
+
+    if (user_msg_size < 3) { // command char, space, first char of the argument
+        printk(KERN_ERR "Phonebook: failed to parse the user buffer -- not enough arguments\n");
+        return 1;
+    }
+
+    switch (user_buffer[0]) { // command char
+    case 'f':
+        index = find_user(user_buffer + 2); // skip the first 2 chars
+        if (index == -1) {
+            printk(KERN_ERR "Phonebook: failed to parse the user buffer -- user not found in 'f'\n");
+            return 1;
+        }
+        user = users[index];
+
+        snprintf(
+            device_buffer,
+            BUFFER_SIZE,
+            "%s %s %s %s %ld\n",
+            user.name,
+            user.surname,
+            user.phone,
+            user.email,
+            user.age
+        );
+        device_msg_size = strlen(device_buffer);
+
+
+        printk(
+            KERN_INFO "Phonebook: found user %s\n",
+            user_buffer + 2
+        );
+        break;
+    case 'a':
+        user = new_user(user_buffer + 2); // skip the first 2 chars
+        if (!user.successfully_created) {
+            printk(KERN_ERR "Phonebook: failed to parse the user buffer -- failed to create a new user\n");
+            return 1;
+        }
+        if (add_user(user)) { // add_user returns 1 on error
+            printk(KERN_ERR "Phonebook: failed to parse the user buffer -- failed to add the created user\n");
+            return 1;
+        }
+
+        printk(
+            KERN_INFO "Phonebook: new user -- '%s', '%s', '%s', '%s', '%ld'\n",
+            user.name,
+            user.surname,
+            user.phone,
+            user.email,
+            user.age
+        );
+        break;
+    case 'd':
+        index = find_user(user_buffer + 2); // skip the first 2 chars
+        if (index == -1) {
+            printk(KERN_ERR "Phonebook: failed to parse the user buffer -- user not found in 'd'\n");
+            return 1;
+        }
+        if (remove_user(index)) { // remove_user returns 1 on error
+            printk(KERN_ERR "Phonebook: failed to parse the user buffer -- failed to remove the user\n");
+            return 1;
+        }
+
+        printk(
+            KERN_INFO "Phonebook: removed user %s\n",
+            user_buffer + 2
+        );
+        break;
+    default:
+        printk(KERN_ERR "Phonebook: failed to parse the user buffer -- unknown command\n");
+        return 1;
+    }
+
+    printk(KERN_INFO "Phonebook: finished parsing the user buffer\n");
     return 0;
 }
 
--- hw1/test.sh
+++ hw1/test.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+# Rudimentary testing utility script, just sanity checks the module
+
 print_dmesg()
 {
     echo "[TEST]: Current dmesg output:"
-    dmesg | tail | grep Phonebook | while read -r line; do
-        echo $line
+    dmesg | tail -n 15 | grep Phonebook | while read -r line; do
+        echo "  $line"
     done
 }
 
@@ -15,14 +17,22 @@ echo "[TEST]: Inserted the module"
 
 print_dmesg
 
-echo "[TEST]: Write test"
-MSG="Hello, world!"
+echo "[TEST]: Write/User creation test"
+MSG="a Ivan Ivanov +75554433 ivan@ivanov.com 30"
 echo $MSG > /dev/phonebook_device
 echo "[TEST] Tried to write '$MSG'"
 
 print_dmesg
 
-echo "[TEST]: Read test"
+echo "[TEST]: Read/User search test"
+echo "f Ivanov" > /dev/phonebook_device
+cat /dev/phonebook_device
+
+print_dmesg
+
+echo "[TEST]: User removal test"
+echo "d Ivanov" > /dev/phonebook_device
+echo "f Ivanov" > /dev/phonebook_device
 cat /dev/phonebook_device
 
 print_dmesg
