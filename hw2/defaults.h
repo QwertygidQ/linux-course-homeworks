050a6888647f69e625d727a916508195f3849130
QwertygidQ <Qwertygid@gmail.com>
2020-02-05 18:51:37 +0300

    Some new defaults

--- hw2/defaults.h
+++ hw2/defaults.h
@@ -4,11 +4,11 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-const size_t   DEFAULT_BOOT_OFFSET  = 1024;
+const size_t   DEFAULT_BOOT_OFFSET  = 1024; // bytes
 const uint16_t DEFAULT_MAGIC        = 0xEF53;
-const uint32_t DEFAULT_BLOCK_SIZE   = 0;
-const uint32_t DEFAULT_TOTAL_BLOCKS = 0;
-const uint32_t DEFAULT_INODE_SIZE   = 0;
+const uint32_t DEFAULT_BLOCK_SIZE   = 8; // bytes
+const uint32_t DEFAULT_TOTAL_BLOCKS = 1024;
+const uint32_t DEFAULT_INODE_SIZE   = 0; // bytes
 const uint32_t DEFAULT_TOTAL_INODES = 0;
 
 #endif
