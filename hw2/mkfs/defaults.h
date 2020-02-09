c8130777f3cee232e40d186521f7a5c5341e1877
QwertygidQ <Qwertygid@gmail.com>
2020-02-09 03:10:58 +0300

    Changed a comment

--- hw2/mkfs/defaults.h
+++ hw2/mkfs/defaults.h
@@ -1,7 +1,7 @@
 #ifndef DEFAULTS_H
 #define DEFAULTS_H
 
-// must be divisible by 8 (32 bits) for indirect addressing, must be greater than DIRECTORY_ENTRY_SIZE
+// must be divisible by 8 (32 bits) for indirect addressing
 #define DEFAULT_BLOCK_SIZE   128   // bytes
 
 #define DEFAULT_TOTAL_BLOCKS 8192  // excluding the superblock
